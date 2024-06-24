374393a3b3124c622d7a80d726ad43dea0566349
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-06-24 05:19:32 +0300

    planning for reports

--- Routes/index.js
+++ Routes/index.js
@@ -6,6 +6,7 @@ const u54Routes = require('./u54');
 const u90Routes = require('./u90');
 
 const allTanksRoutes = require('./all-tanks');
+const reportsRoutes = require('./reports');
 const UsersRoutes = require('./users');
 const AuthRoutes = require('./auth');
 
@@ -16,6 +17,7 @@ module.exports = {
   u54Routes,
   u90Routes,
   allTanksRoutes,
+  reportsRoutes,
   UsersRoutes,
   AuthRoutes,
 };
---
+++ Routes/reports/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Unit 52
+// 1. get tank info for a specific day
+// 2. get all tanks for a specific day
+// 3. get tank info for a specific date range.
+// 4. get all tanks for a specific date range.
+// 5. get crude oil for  a specific day.
+// 6. get crude oil for  a specific date range.
+// 7. get naturl gas for  a specific day.
+// 8. get naturl gas for  a specific date range.
+// 9. get blending for  a specific day.
+// 10. get blending for  a specific date range.
+
+// Unit 53
+// 1. get tank info for a specific day
+// 2. get all tanks for a specific day
+// 3. get Asphalt transport for a specific day
+// 4. get  Asphalt transport  for a specific date range
+
+// Unit 54
+// 1. get Solid Sulphur Storeage for a specific day
+// 2. get Solid Sulphur Storeage for a specific date range
+// 3. get Solid Sulphur Production for a specific day
+// 4. get Solid Sulphur Production for a specific date range
+// 5. get Solid Sulphur Transport for a specific day
+// 6. get Solid Sulphur Transport for a specific date range
+
+// Unit 90
+// 1. get tank info for a specific day
+// 2. get all tanks for a specific day
+// 3. get tank info for a specific date range.
+// 4. get all tanks for a specific date range.
+// 5. get Diesel Pumping for a specific day
+// 6. get Diesel Pumping for a specific date range
+// 7. get Kerosene Pumping for a specific day
+// 8. get Kerosene Pumping for a specific date range
+// 9. get PG Pumping for a specific day
+// 10. get PG Pumping for a specific date range
+// 11. get RG Pumping for a specific day
+// 12. get RG Pumping for a specific date range
+// 13. get ATK Transport for a specific day
+// 14. get ATK Transport for a specific date range
+// 15. get PG Transport for a specific day
+// 16. get PG Transport for a specific date range
+// 17. get RG Transport for a specific day
+// 18. get RG Transport for a specific date range
+// 19. get HFO Transport for a specific day
+// 20. get HFO Transport for a specific date range
+// 21. get HFO Transport for a specific day and side a specific
+// 22. get HFO Transport for a specific date range and side a specific
+
+// REPORTS
+// 1. get a full report for a specific day.
+
+module.exports = router;
