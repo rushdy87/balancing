df94b4c52e99c5d263a07ab3d58013a309e39baf
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-07-14 04:03:21 +0300

    Unit 54 notes

--- Routes/unit54/index.js
+++ Routes/unit54/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const u54StorageRoutes = require('./storage');
 const SolidSulphurTransportRoutes = require('./transport');
 const SolidSulphurProductionRoutes = require('./production');
+const NotesRoutes = require('./notes');
 
 // /api/u52/
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.use('/store', u54StorageRoutes);
 router.use('/transport', SolidSulphurTransportRoutes);
 router.use('/production', SolidSulphurProductionRoutes);
+router.use('/notes', NotesRoutes);
 
 module.exports = router;
