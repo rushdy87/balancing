6d54d7cb32a688c320207623b478676dc366bf30
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-07-19 19:11:22 +0300

    Working on u52 crude and gas in client

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,3 +5,5 @@ export { default as ReportView } from './ReportView/ReportView';
 
 export { default as TanksInputs } from './TanksInputs/TanksInputs';
 export { default as Blending } from './Blending/Blending';
+export { default as CrudeOil } from './CrudeOil/CrudeOil';
+export { default as NaturalGas } from './NaturalGas/NaturalGas';
--- client/src/pages/Unit52/Unit52.jsx
+++ client/src/pages/Unit52/Unit52.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Blending, Button, Datepicker, TanksInputs } from '../../components';
+import {
+  Blending,
+  Button,
+  CrudeOil,
+  Datepicker,
+  NaturalGas,
+  TanksInputs,
+} from '../../components';
 import { getTanksByUnit } from '../../api/tanks';
 import './Unit52.scss';
 
@@ -33,10 +40,26 @@ const Unit52 = () => {
     hfo: 0,
   });
 
+  // Crude Oil
+  const [crudeOil, setCrudeOil] = useState({
+    reservoir_m3: 0,
+    reservoir_bbl: 0,
+    receiving: 0,
+    sending: 0,
+  });
+
+  // Natural Gas
+  const [naturalGas, setNaturalGas] = useState({
+    receiving_m3: 0,
+    receiving_mscf: 0,
+  });
+
   const handleChange = (event) => {
     event.preventDefault();
     console.log(tanks);
     console.log(blendingQuantities);
+    console.log(crudeOil);
+    console.log(naturalGas);
   };
 
   return (
@@ -52,12 +75,29 @@ const Unit52 = () => {
           <TanksInputs tanks={tanks} setTanks={setTanks} />
         </div>
 
-        <div className='u52_blending'>
-          <h3 className='u52_subheading'>مزج المنتجات</h3>
-          <div>
-            <Blending setBlendingQuantities={setBlendingQuantities} />
+        <div className='blending_crude_gas'>
+          <div className='u52_blending'>
+            <h3 className='u52_subheading'>مزج المنتجات</h3>
+            <div>
+              <Blending setBlendingQuantities={setBlendingQuantities} />
+            </div>
+          </div>
+
+          <div className='u52_crudeOil'>
+            <h3 className='u52_subheading'>النفط الخام</h3>
+            <div>
+              <CrudeOil setCrudeOil={setCrudeOil} />
+            </div>
+          </div>
+
+          <div className='u52_gas'>
+            <h3 className='u52_subheading'>الغاز الطبيعي</h3>
+            <div>
+              <NaturalGas setNaturalGas={setNaturalGas} />
+            </div>
           </div>
         </div>
+
         <Button type='sumit'>معاينة</Button>
       </form>
     </div>
