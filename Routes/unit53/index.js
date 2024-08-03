d79a010bab696d4b804e78288cabaefdbbb85255
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-08-03 07:07:32 +0300

    U53 admin backend

--- Routes/unit53/index.js
+++ Routes/unit53/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const u53TanksRoutes = require('./tanks');
 const PavingAsphaltTransportRoutes = require('./transport');
 const NotesRoutes = require('./notes');
+const AdminRoutes = require('./admin');
 
-// /api/u52/
+// /api/u53/
 const router = express.Router();
 
 router.use('/tanks', u53TanksRoutes);
 router.use('/transport', PavingAsphaltTransportRoutes);
 router.use('/notes', NotesRoutes);
+router.use('/admin', AdminRoutes);
 
 module.exports = router;
---
+++ controllers/unit53/admin.js
@@ -0,0 +1,58 @@
+const { Unit53Tank, PavingAsphaltTransport } = require('../../models');
+const {
+  validateInput,
+  formatDate,
+  handleError,
+  findTanksByDate,
+  checkAuthorization,
+  findTankInfo,
+  findTransport,
+} = require('../../utils');
+
+exports.getAllData = async (req, res, next) => {
+  const { day } = req.params;
+  const { userData } = req;
+
+  if (!validateInput(req.params, ['day'], next)) return;
+
+  const formattedDate = formatDate(day);
+  try {
+    checkAuthorization(userData, 'u53', next);
+
+    const unit53Data = {};
+
+    let tanks = await findTanksByDate(Unit53Tank, formattedDate);
+    if (!tanks) {
+      tanks = [];
+    } else {
+      tanks = await Promise.all(
+        tanks.map(async (tank) => {
+          const { low_level } = await findTankInfo(tank.tag_number);
+          const tov = tank.pumpable + low_level;
+          return {
+            tag_number: tank.tag_number,
+            product: tank.product,
+            tov,
+            day: tank.day,
+            isConfirmed: tank.isConfirmed,
+          };
+        })
+      );
+    }
+    unit53Data.tanks = tanks;
+
+    let pavingAsphaltTransport = await findTransport(
+      PavingAsphaltTransport,
+      formattedDate
+    );
+    if (!pavingAsphaltTransport) {
+      pavingAsphaltTransport = {};
+    }
+    unit53Data.pavingAsphaltTransport = pavingAsphaltTransport;
+
+    res.status(200).json(unit53Data);
+  } catch (error) {
+    console.log(error);
+    handleError(next, error.message, 500);
+  }
+};
