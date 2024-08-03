7e1a43413df217bce670d1cd2c78297c814f9a28
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-08-03 03:48:39 +0300

    workin in natural gas

--- controllers/unit52/natural-gas.js
+++ controllers/unit52/natural-gas.js
@@ -82,8 +82,11 @@ exports.addNaturalGas = async (req, res, next) => {
       await deleteNaturalGas(formattedDate);
     }
 
+    const receiving_m3 = Math.round(data.receiving_mscf / (1177 * 24));
+
     const gasVolumes = addNaturalGasVolumes({
       ...data,
+      receiving_m3,
       day: formattedDate,
       userId: userData.id,
     });
